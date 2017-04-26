cd9e42f8a5911b95bbe4caa52a929ebef275a783
Estelle Sillari-Owen <emso18@gmail.com>
2017-04-26 18:58:32 -0400

    more efficient filtering

--- twitch.js
+++ twitch.js
@@ -1,11 +1,13 @@
-var channels = ["brunofin", "comster404", "cretetion", "ESL_SC2", "freecodecamp", "habathcx", "noobs2ninjas", "OgamingSC2", "RobotCaleb", "storbeck"];
+var channels = ["cretetion", "ESL_SC2", "freecodecamp", "habathcx", "noobs2ninjas", "OgamingSC2", "RobotCaleb", "storbeck", "brunofin", "comster404"];
+var selected = "all";
+var search = "";
 
 function getChannel(each){
   $.getJSON("https://wind-bow.glitch.me/twitch-api/channels/" + channels[each], function(users){
       if(users.error !== undefined){
         $(".user" + each).html(channels[each]);
         $(".logo" + each).html("<img class='rounded-circle' src='https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png'>");
-        $(".icon" + each).html("<i class='fa fa-exclamation-circle dne float-right' aria-hidden='true'></i>");
+        $(".icon" + each).html("<i class='fa fa-exclamation-circle dne float-right' title='No account found' aria-hidden='true'></i>");
         $(".c" + each).addClass("all");
       }
       else {
@@ -17,20 +19,19 @@ function getChannel(each){
         }
         $.getJSON("https://wind-bow.glitch.me/twitch-api/streams/" + channels[each], function(users){
           if(users.stream === null){
-            $(".user" + each).html(channels[each] + "<i class='fa fa-circle offline float-right' aria-hidden='true'></i>");
+            $(".user" + each).html(channels[each] + "<i class='fa fa-circle offline float-right' title='Offline' aria-hidden='true'></i>");
             $(".c" + each).addClass("off");
             $(".c" + each).addClass("all");
           }
           else {
             $(".user" + each).html(channels[each] + " : " + users.stream.game);
-            $(".icon" + each).html("<i class='fa fa-circle online float-right' aria-hidden='true'></i>");
+            $(".icon" + each).html("<i class='fa fa-circle online float-right' title='Online' aria-hidden='true'></i>");
             $(".c" + each).addClass("on")
             $(".c" + each).addClass("all");
           }
         });
       }
   });
-  $(".c" + each).addClass(channels[each]);
 }
 
 function getInfo(){
@@ -39,48 +40,32 @@ function getInfo(){
   }
 }
 
-function getActive(){
-  if($(".onBtn").hasClass("active")){
-    return "on";
-  }
-  else if($(".offBtn").hasClass("active")){
-    return "off";
-  }
-  else {
-    return "all";
-  }
-}
-
 function getFilters(){
   $(".offBtn").click(function(){
-    var search = $("input:text").val().toLowerCase();
-    var selected = "off";
+    selected = "off";
     filterResults(search, selected);
   });
   $(".onBtn").click(function(){
-    var search = $("input:text").val().toLowerCase();
-    var selected = "on";
+    selected = "on";
     filterResults(search, selected);
   });
   $(".allBtn").click(function(){
-    var search = $("input:text").val().toLowerCase();
-    var selected = "all";
+    selected = "all";
     filterResults(search, selected);
   });
   $("input").keyup(function(){
-    var search = $("input:text").val().toLowerCase();
-    var selected = getActive();
+    search = $("input:text").val().toLowerCase();
     filterResults(search, selected);
   });
 }
 
 function filterResults(search, selected){
   for(each in channels){
-    if(channels[each].toLowerCase().match(search) !== null && $("." + channels[each]).hasClass(selected)){
-      $("." + channels[each]).show();
+    if($(".user" + each).text().toLowerCase().match(search) !== null && $(".c" + each).hasClass(selected)){
+      $(".c" + each).show();
     }
     else {
-      $("." + channels[each]).hide();
+      $(".c" + each).hide();
     }
   }
 }
